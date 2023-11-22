4827278bf97e723fb2fb864cc876d7bc8d262094
Marcus Bergo <mbergo@modeln.com>
2023-11-22 06:07:15 -0300

    - Adding some funny comments (#2)
    This adds some information about the code.
    
    Co-authored-by: Marcus Bergo <mbergo@modeln>

--- src/hexa.c
+++ src/hexa.c
@@ -5,7 +5,7 @@
 
 #define NUM_THREADS 6
 
-// Structure to hold download parameters
+// A struct to hold download details - because who doesn't like being organized?? 📚
 typedef struct {
     long start;
     long end;
@@ -13,13 +13,12 @@ typedef struct {
     char url[1024];
 } download_part_args;
 
-// Function to write data received from HTTP request
+// A function so small yet so crucial, like the tiny screw in your glasses 🤓
 size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
-    size_t written = fwrite(ptr, size, nmemb, stream);
-    return written;
+    return fwrite(ptr, size, nmemb, stream);
 }
 
-// Function to download part of a file
+// Our heroic function downloading parts of the file, like a ninja in the night 🥷
 void *download_part(void *arguments) {
     download_part_args *args = (download_part_args *)arguments;
     
@@ -29,7 +28,7 @@ void *download_part(void *arguments) {
     char range[64];
     char outfilename[64];
 
-    // Prepare file name and range
+    // Crafting filenames and ranges like an artisanal baker 🍞
     sprintf(outfilename, "part%d", args->partNum);
     sprintf(range, "%ld-%ld", args->start, args->end);
 
@@ -37,16 +36,16 @@ void *download_part(void *arguments) {
     if (curl) {
         fp = fopen(outfilename, "wb");
 
-        // Set CURL options
+        // Setting all the dials and knobs on our CURL machine 🎛️
         curl_easy_setopt(curl, CURLOPT_URL, args->url);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
         curl_easy_setopt(curl, CURLOPT_RANGE, range);
 
-        // Perform the download
+        // Off to the races! 🏁
         res = curl_easy_perform(curl);
-
-        // Cleanup
+        
+        // Cleaning up after the party 🧹
         fclose(fp);
         curl_easy_cleanup(curl);
     }
@@ -54,13 +53,14 @@ void *download_part(void *arguments) {
     return NULL;
 }
 
+// The grand conductor of our multi-threaded orchestra 🎼
 int main(int argc, char **argv) {
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <URL>\n", argv[0]);
-        return -1;
+        exit(-1);
     }
 
-    // Get the size of the file
+    // First, we ask politely how big the file is 🤔
     CURL *curl = curl_easy_init();
     double filesize = 0.0;
     if(curl) {
@@ -71,43 +71,43 @@ int main(int argc, char **argv) {
         if(curl_easy_perform(curl) == CURLE_OK) {
             if(curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD, &filesize) != CURLE_OK) {
                 perror("curl_easy_getinfo");
-                return -1;
+                exit(-1);
             }
         } else {
             perror("curl_easy_perform");
-            return -1;
+            exit(-1);
         }
         curl_easy_cleanup(curl);
     }
 
-    // Prepare thread data
     pthread_t threads[NUM_THREADS];
     download_part_args args[NUM_THREADS];
     long part_size = (long)(filesize / NUM_THREADS);
 
-    // Start threads
+    // Spinning up threads like a DJ spins records 🎧
     for (int i = 0; i < NUM_THREADS; i++) {
         args[i].start = i * part_size;
         args[i].end = (i + 1) * part_size - 1;
-        if (i == NUM_THREADS - 1) args[i].end += (long)(filesize % NUM_THREADS);  // Last part gets remainder
+        if (i == NUM_THREADS - 1) args[i].end += (long)(filesize % NUM_THREADS);  // Last thread gets the leftovers 🍔
         args[i].partNum = i;
         strcpy(args[i].url, argv[1]);
 
+        // Unleashing the thread hounds!
         if (pthread_create(&threads[i], NULL, download_part, (void *)&args[i])) {
             perror("pthread_create");
-            return -1;
+            exit(-1);
         }
     }
 
-    // Join threads
+    // Waiting for our thread hounds to return 🐕
     for (int i = 0; i < NUM_THREADS; i++) {
         if (pthread_join(threads[i], NULL)) {
             perror("pthread_join");
-            return -1;
+            exit(-1);
         }
     }
 
-    // Concatenate parts
+    // Time to stitch it all together, like a digital Frankenstein 🧟
     FILE *fp = fopen("downloaded_file", "wb");
     for (int i = 0; i < NUM_THREADS; i++) {
         char outfilename[64];
@@ -116,7 +116,7 @@ int main(int argc, char **argv) {
         FILE *part_fp = fopen(outfilename, "rb");
         if (part_fp == NULL) {
             perror("fopen");
-            return -1;
+            exit(-1);
         }
 
         char buffer[8192];
@@ -125,11 +125,11 @@ int main(int argc, char **argv) {
             fwrite(buffer, 1, bytes, fp);
         }
         fclose(part_fp);
-
-        // Remove the part file
-        remove(outfilename);
+        remove(outfilename);  // Cleaning up our temporary mess 🗑️
     }
     fclose(fp);
 
+    // Victory dance, because we just downloaded a file in style 💃🕺
+    printf("Download complete! File assembled and ready for action.\n");
     return 0;
 }
